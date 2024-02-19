9263b1a3897df50e9ed39c9b3a8498e4d0e7ddb3
manelreghima <manelreghima@gmail.com>
2024-02-19 15:49:36 +0100

    add re-deploy script

---
+++ re-deploy.sh
@@ -0,0 +1,17 @@
+
+# Activate virtual environment if applicable
+echo "Activate virtual environment..."
+#source venv/Scripts/activate
+source venv/bin/activate
+
+# Install or update dependencies
+echo "Installing/updating dependencies..."
+pip install -r requirements.txt
+
+# Pull latest changes from Git
+echo "Pulling latest changes from Git..."
+git pull
+
+# Start Streamlit app
+echo "Starting Streamlit app..."
+streamlit run Home.py
